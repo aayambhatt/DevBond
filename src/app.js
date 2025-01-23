56c5aef0da2342839a0673dca34ff326e788cdfb
aayambhatt <aayambhatt23@gmail.com>
2025-01-23 15:45:10 +0530

    did get, post,delete on /user

--- src/app.js
+++ src/app.js
@@ -4,10 +4,28 @@ const express = require("express");
 // Creating an instance of the express application
 const app = express();
 
-//handle incoming requests and send a response
-app.use( "/", (req,res)=>{
-    res.send("Hello from the server, nodemon");
+//this will only handle GET call to /user
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Aayam", lastName: "Bhatt"});
 })
+
+app.post("/user", (req,res)=>{
+    //saving data to DB
+    res.send("Data successfully saved");
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("Deleted successfully");
+
+})
+
+// handle incoming requests and send a response
+// this will match all the http method api calls to /test
+app.use("/test", (req,res)=>{
+        res.send("This is /test");
+
+})
+
 
 app.listen(2626, ()=>{
     console.log("Server is succeful on port 2626");
