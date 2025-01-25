2fdbc44a1fd4684ccd2d1d8f58cfbf0a577de0ea
aayambhatt <aayambhatt23@gmail.com>
2025-01-25 16:56:07 +0530

    Multiple routes

--- src/app.js
+++ src/app.js
@@ -4,29 +4,27 @@ const express = require("express");
 // Creating an instance of the express application
 const app = express();
 
-//this will only handle GET call to /user
-app.get("/user", (req,res)=>{
-    res.send({firstName:"Aayam", lastName: "Bhatt"});
-})
+app.get(
+  "/user",
+  (req, res, next) => {
+    //Route handler
+    // res.send("Route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Route handler 2");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Route handler 3");
+    next();
+  },
+  (req, res,next) => {
+    // res.send("Route handler 4");
+    next();
+  }
+);
 
-app.post("/user", (req,res)=>{
-    //saving data to DB
-    res.send("Data successfully saved");
-})
-
-app.delete("/user", (req,res)=>{
-    res.send("Deleted successfully");
-
-})
-
-// handle incoming requests and send a response
-// this will match all the http method api calls to /test
-app.use("/test", (req,res)=>{
-        res.send("This is /test");
-
-})
-
-
-app.listen(2626, ()=>{
-    console.log("Server is succeful on port 2626");
+app.listen(2626, () => {
+  console.log("Server is succeful on port 2626");
 });
