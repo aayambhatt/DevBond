073e9fa5988cfdaf9bfe1e308130d5a6f6cf6d3c
aayambhatt <aayambhatt23@gmail.com>
2025-01-27 17:51:05 +0530

    signup-api

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
+
 const app = express();
 
+app.post("/signup", async (req,res)=>{
 
-app.get("/getuserdata", (req,res)=> {
-  try{
-    throw new Error("Data not allowed");
-    res.send("User data is displayed");
-  }
-  catch(err){
-    res.status(500).send("Please contact support team");
-  }
-  
+  // creating a new instance of the User model
+  const user = new User({
+    firstName: "Ruru",
+    lastName: "Sen",
+    emailID: "RuruSen@gmail.com",
+    password: "Ruru@1234"
+});
+
+try{
+await user.save();
+res.send("User added successfully");
+} catch(err){
+  res.status(400).send("Error saving the error")
+}
+
+
 })
 
 
 
+connectDB()
+.then(()=>{
+console.log("DB connection is successfull");
 app.listen(2626, () => {
   console.log("Server is successful on port 2626");
 });
+})
+.catch((err)=>{
+console.error("DB cannot be connected");
+})
+
+
+
