89e9d2174bd5723873018932406ef3d526e8414f
aayambhatt <aayambhatt23@gmail.com>
2025-02-03 14:05:01 +0530

    schema-validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,53 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: { 
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+
     },
     lastName: {
         type: String
     },
     emailID: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+
     },
     age: {
         type: Number
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male","female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.geographyandyou.com/images/user-profile.png"
+    },
+    about: {
+        type: String,
+        default: "This is a default description of the user", 
+    },
+    skills: {
+        type: [String],
     }
-})
+}, {
+    timestamps: true,
+   }
+);  
 
 
 module.exports =  mongoose.model("User", userSchema);
