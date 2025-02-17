1a257e984aaf8d23e0a1e4e7b19f9502be8439e7
aayambhatt <aayambhatt23@gmail.com>
2025-02-17 12:00:00 +0000

    Auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,33 @@
-const adminAuth = (req,res,next)=>{
-    const token = "xyz";
-   const isAuthorized = token === "xyz";
- 
-   if(!isAuthorized){
-     res.status(401).send("Data not allowed")
-   }
-   else{
-     next();
-   }
- 
+const jwt = require("jsonwebtoken"); 
+const User = require("../models/user.js");
+
+ const userAuth = async (req,res,next)=>{
+ try{ // read the token from the request cookies
+  const {token} = req.cookies;
+  if(!token){
+    throw new Error("Token is not valid");
+  }
+  // validate the token
+  const decodedObj = await jwt.verify(token, "DEVBOND@123");
+
+  // get id 
+  const {_id} = decodedObj;
+
+  const user = await User.findById(_id);
+  if(!user){
+    throw new Error("User not found");
+  }
+
+  req.user = user;
+
+  next();
+}
+
+catch(err){
+  res.status(400).send("ERROR" + err.message);
+}
   
- }
-
-
- const userAuth = (req,res,next)=>{
-    const token = "xyz";
-   const isAuthorized = token === "xyz";
- 
-   if(!isAuthorized){
-     res.status(401).send("Data not allowed")
-   }
-   else{
-     next();
-   }
- 
   
- }
+ };
 
- module.exports = { adminAuth, userAuth }
+ module.exports = { userAuth };
